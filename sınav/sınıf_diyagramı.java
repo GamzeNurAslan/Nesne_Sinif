82a4956dfae468b244695f65fcda22d6541e90b9
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-01-31 20:42:38 +0300

    sınıf_diyagramı.java

---
+++ sınav/sınıf_diyagramı.java
@@ -0,0 +1,20 @@
+class Egitim{
+public void Calis(){}
+}
+class Ogrenci extends Egitim{
+private void Dinle(){}
+}
+class Ogretmen extends Egitim{
+public void Anlat(){}
+}
+class YLisanOgrenci extends Ogrenci{
+public void Arastir(){}
+public void OdevYap(){}
+}
+class ArasGor extends Ogretmen{
+public void Arastir(){}
+public void ProjeYap(){}
+}
+public class Main{
+public static void main(String [] args){}
+}
